0cdc5d80aa915910ea647296c5595ac5f76274d6
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-11-05 11:26:56 -0300

    Show language hints instructions on how-to-play

--- src/components/HelpModal/HelpModal.js
+++ src/components/HelpModal/HelpModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FocusTrap } from "focus-trap-react";
+import { FLAG_COMPONENTS } from "../../constants";
 
 function HelpModal({ setShowHelpModal }) {
   React.useEffect(() => {
@@ -19,6 +20,9 @@ function HelpModal({ setShowHelpModal }) {
       document.body.setAttribute("aria-hidden", "false");
     };
   }, []);
+
+  const CorrectFlagComponent = FLAG_COMPONENTS["pt-br"];
+  const IncorrectFlagComponent = FLAG_COMPONENTS["en"];
 
   return (
     <FocusTrap>
@@ -52,6 +56,12 @@ function HelpModal({ setShowHelpModal }) {
             <p>
               And choose the length of the word you want to guess,
               ranging from 3 to 10 letters.
+            </p>
+            <p>
+              Toggle <strong>Show Language Hint</strong> to display if
+              the guessed word language matches the answer language,
+              which helps you identify the language the word belongs
+              to during the game.
             </p>
             <p>And then, Start Game.</p>
             <br />
@@ -138,6 +148,125 @@ function HelpModal({ setShowHelpModal }) {
               not in the word.
             </p>
             <p>Words can have repeated letters.</p>
+            <br />
+            <p>
+              If you enabled <strong>Show Language Hint</strong>,
+              you'll get feedback on whether your guess's language
+              matches the answer's language.
+            </p>
+            <div className="example">
+              <span aria-label="letter A" className="cell">
+                A
+              </span>
+              <span aria-label="letter M" className="cell">
+                M
+              </span>
+              <span aria-label="letter O" className="cell">
+                O
+              </span>
+              <span aria-label="letter R" className="cell">
+                R
+              </span>
+              <span aria-label="letter A" className="cell">
+                A
+              </span>
+              <div
+                className="languages-wrapper"
+                style={{ "--word-length": 2.3 }}
+              >
+                <span className="found-languages">
+                  <span className="round-cell correct-language">
+                    <CorrectFlagComponent className="flag-icon" />
+                    <span className="visually-hidden">Português</span>
+                  </span>
+                </span>
+              </div>
+            </div>
+            <p>
+              The guess <strong>A M O R A</strong> language
+              (Português) matches the answer's language{" "}
+              <span className="flag-example round-cell correct-language">
+                <CorrectFlagComponent className="flag-icon" />
+              </span>
+              .
+            </p>
+            <div className="example">
+              <span aria-label="letter L" className="cell">
+                L
+              </span>
+              <span aria-label="letter O" className="cell">
+                O
+              </span>
+              <span aria-label="letter V" className="cell">
+                V
+              </span>
+              <span aria-label="letter E" className="cell">
+                E
+              </span>
+              <span aria-label="letter R" className="cell">
+                R
+              </span>
+              <div
+                className="languages-wrapper"
+                style={{ "--word-length": 2.3 }}
+              >
+                <span className="found-languages">
+                  <span className="round-cell incorrect-language">
+                    <IncorrectFlagComponent className="flag-icon" />
+                    <span className="visually-hidden">Português</span>
+                  </span>
+                </span>
+              </div>
+            </div>
+            <p>
+              The guess <strong>L O V E R</strong> language (English)
+              doesn't match the answer's language{" "}
+              <span className="flag-example round-cell incorrect-language">
+                <IncorrectFlagComponent className="flag-icon" />
+              </span>
+              .
+            </p>
+            <div className="example">
+              <span aria-label="letter M" className="cell">
+                M
+              </span>
+              <span aria-label="letter O" className="cell">
+                O
+              </span>
+              <span aria-label="letter V" className="cell">
+                V
+              </span>
+              <span aria-label="letter E" className="cell">
+                E
+              </span>
+              <span aria-label="letter R" className="cell">
+                R
+              </span>
+              <div
+                className="languages-wrapper"
+                style={{ "--word-length": 2.3 }}
+              >
+                <span className="found-languages misplaced">
+                  <span className="round-cell misplaced-language">
+                    <CorrectFlagComponent className="flag-icon" />
+                    <span className="visually-hidden">Português</span>
+                  </span>
+                  <span className="round-cell misplaced-language">
+                    <IncorrectFlagComponent className="flag-icon" />
+                    <span className="visually-hidden">English</span>
+                  </span>
+                </span>
+              </div>
+            </div>
+            <p>
+              One of the guess <strong>M O V E R</strong> possible
+              languages (Português and English) matches the answer's
+              language{" "}
+              <span className="flag-example round-cell misplaced-language">
+                <CorrectFlagComponent className="flag-icon" />
+              </span>
+              .
+            </p>
             <br />
             <p>A new word appears every page reload.</p>
           </div>
