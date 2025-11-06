e22e34d9a3854f2fa0bb9b34b92883a1684de408
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-11-06 17:53:17 -0300

    Add skip to content yay

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import Game from "../Game";
 import Header from "../Header";
+import SkipToContent from "../SkipToContent";
 
 function App() {
   return (
     <div className="wrapper">
+      <SkipToContent />
       <Header />
-      <div className="game-wrapper">
+      <div className="game-wrapper" id="game-content">
         <Game />
       </div>
     </div>
---
+++ src/components/SkipToContent/SkipToContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SkipToContent() {
+  return (
+    <a href="#game-content" className="skip-link">
+      Skip to content
+    </a>
+  );
+}
+
+export default SkipToContent;
